809042ec87fa65ae2b75dbff066e508074b9c93e
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-09-29 19:22:41 +0530

    fix : config errror

--- todo/vite.config.js
+++ todo/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    loader: 'jsx', // Treat .js files as .jsx
-    include: /src\/.*\.js$/, // Include only the files from src with .js extension
-  },
   plugins: [react()],
-})
+});
